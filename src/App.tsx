a1ce5d145bffa13d80b8e75fade6a551253f2ff7
oscarigo <59118913+Os-humble-man@users.noreply.github.com>
2024-11-05 20:42:00 +0200

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"coral"}>
+        Nav
+      </GridItem>
+      <GridItem
+        area={"aside"}
+        bg={"gold"}
+        display={{ base: "none", lg: "block" }}
+      >
+        aside
+      </GridItem>
+      <GridItem area={"main"} bg={"dodgerblue"}>
+        Nav
+      </GridItem>
+    </Grid>
   );
 }
 
