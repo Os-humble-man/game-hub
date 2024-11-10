2cc0e5cb98aaad1d9b30164dc38015d75de512f4
oscarigo <59118913+Os-humble-man@users.noreply.github.com>
2024-11-10 16:36:01 +0200

    Refactor : game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,13 +11,12 @@ export default function GameGrid({ gameQuery }: Props) {
   const { error, data, isLoading } = useGames(gameQuery);
   const skeletons = Array(24).fill(0);
 
+  if (error)
+    return <p className="col-span-full text-center text-red-500">{error}</p>;
+
   return (
     <div className="w-full ">
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-        {error && (
-          <p className="col-span-full text-center text-red-500">{error}</p>
-        )}
-
         {isLoading &&
           skeletons.map((_, index) => <GameCardSkeleton key={index} />)}
 
